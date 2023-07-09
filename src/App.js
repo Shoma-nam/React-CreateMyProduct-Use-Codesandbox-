442fb6dbe1b89ead5beb19738ad8749d48bd3c4f
Shoma-nam <132134861+Shoma-nam@users.noreply.github.com>
2023-07-09 17:33:06 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+export default function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1>arsenal</h1>
+        <h2>メンバー紹介</h2>
+      </div>
+      <div>
+        <div>
+          <h3>ゴールキーパー (GK)</h3>
+          <p>抜粋文</p>
+        </div>
+        <div>
+          <h3>ディフェンダー (DF)</h3>
+          <h4>センターバック</h4>
+          <p>抜粋文</p>
+          <h4>サイドバック</h4>
+          <p>抜粋文</p>
+        </div>
+        <div>
+          <h3>ミッドフィールダー (MF)</h3>
+          <h4>攻撃的MF</h4>
+          <p>抜粋文</p>
+          <h4>守備的MF</h4>
+          <p>抜粋文</p>
+        </div>
+        <div>
+          <h2>記事３</h2>
+          <p>抜粋文</p>
+        </div>
+      </div>
+      　　　
+    </div>
+  );
+}
